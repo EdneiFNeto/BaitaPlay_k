5bbf5d87d161eef836e98d2ba6adff6b00375adf
Ednei de Freitas Neto <ednei.freitas@moebius.com.br>
2019-05-25 20:51:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/baitaplay_k/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.baitaplay_k
+
+import android.os.Bundle
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.toolbar.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        setSupportActionBar(my_toolbar)
+        val actionbar = supportActionBar
+        actionbar?.setDisplayShowTitleEnabled(false)
+
+        init()
+
+    }
+
+    private fun init() {
+        val manager = supportFragmentManager
+        val tx = manager.beginTransaction()
+        tx.replace(R.id.framelayput_video, VideoFragment());
+    }
+}
